12ee9d5af823c8e1c08beec48530123241950d94
justine.meuret <jumeuret@e.email>
2023-01-22 18:18:15 +0100

    :sparkles: Génération automatique de la température du CapteurCPU :sparkles:

--- src/modele/GenerateurTemperatureCPU.java
+++ src/modele/GenerateurTemperatureCPU.java
@@ -1,7 +1,12 @@
 package modele;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class GenerateurTemperatureCPU extends GenerateurTemperature {
 
@@ -10,9 +15,40 @@ public class GenerateurTemperatureCPU extends GenerateurTemperature {
     }
 
     public void change() throws IOException {
-        ProcessBuilder bash = new ProcessBuilder();
-        bash.command("cd", "cd /sys/class/thermal/", "ls | grep 'thermal_zone'");
-        Process result = bash.start();
-        System.out.println(result.getOutputStream());
+        double temperature = 0;
+        try {
+
+            File fichier = new File("/sys/class/thermal");
+            List<File> thermal_zones = new ArrayList<>();
+            if (fichier.isDirectory()) {
+                for (File fils : fichier.listFiles()) {
+                    if (fils.getName().contains("thermal_zone")) {
+                        thermal_zones.add(fils);
+                        System.out.println("th : " + fils);
+                    }
+                }
+            }
+
+            for (File fich : thermal_zones) {
+                for (File file : fich.listFiles()) {
+                    if (file.getName().equals("type")) {
+                        Scanner reader = new Scanner(file);
+                        if (reader.nextLine().equals("x86_pkg_temp")) {
+                            File temp = new File(String.format("/sys/class/thermal/%s/temp", fich.getName()));
+                            Scanner lecteur = new Scanner(temp);
+                            String ligne = lecteur.nextLine();
+                            temperature = Double.parseDouble(ligne);
+                            temperature = temperature / 1000;
+                            lecteur.close();
+                        }
+                        reader.close();
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        capteurTemp.setTemperature(temperature);
+        System.out.println(temperature);
     }
 }
